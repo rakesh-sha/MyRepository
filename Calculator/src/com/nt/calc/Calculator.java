cc29cd086ba7c540501ba672f2c12c4b783dd303
rakes <rakes@DESKTOP-H0IU23S>
2020-04-10 22:36:03 +0530

    Modified by dev1 add-method

--- Calculator/src/com/nt/calc/Calculator.java
+++ Calculator/src/com/nt/calc/Calculator.java
@@ -2,7 +2,16 @@ package com.nt.calc;
 
 public class Calculator {
 	public long res;
+	public long add(int a,int b){
+		
+		return a+b;
+	}
 	public static void main(String[] args) {
+		Calculator calc=new Calculator();
+		calc.res=calc.add(10, 20);
+		System.out.println("Result is : "+calc.res);
+		
+		
 		
 	}
 
